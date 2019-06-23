2f253b1ad31ebc76134e64a64624b355f139f90c
smeshkov <smeshkow@gmail.com>
2019-06-23 15:37:53 +1000

    Changed logic of CLI to not rely on tagified text and use raw text lines instead

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -6,15 +6,14 @@ import (
 	"os"
 	"strings"
 
+	"github.com/gpestana/htmlizer"
 	"github.com/zoomio/inout"
-	"github.com/zoomio/tagify"
-	"github.com/zoomio/tagify/processor"
 
 	"github.com/smeshkov/lsh"
 )
 
 var (
-	// lsh command
+	// LSH command
 	lshCmd       = flag.NewFlagSet("lsh", flag.ExitOnError)
 	lshSources   = lshCmd.String("s", "", "List of sources separated by comma.")
 	lshNumHashes = lshCmd.Int("nh", 100, "Number of hash functions.")
@@ -78,18 +77,7 @@ func parseCommand(cmd *flag.FlagSet) {
 }
 
 func getShingles(source string, doKShingle bool) []string {
-	reader, err := inout.New(source)
-	if err != nil {
-		fmt.Printf("can't read source %s: %v\n", source, err)
-		return []string{}
-	}
-	lines, err := reader.ReadLines()
-	if err != nil {
-		fmt.Printf("can't fetch contents of %s: %v\n", source, err)
-		return []string{}
-	}
-
-	textLines := tagify.ToStrings(processor.ParseHTML(lines, false, false))
+	textLines := getTextLines(source)
 
 	if doKShingle {
 		return lsh.KShingle(textLines, *simKShingles)
@@ -98,17 +86,7 @@ func getShingles(source string, doKShingle bool) []string {
 	return lsh.Shingle(textLines)
 }
 
-func shingleSets(sources string, doKShingle bool) [][]string {
-	if sources == "" {
-		printUsage()
-		os.Exit(4)
-	}
-
-	sourcesList := strings.Split(sources, ",")
-	if len(sources) < 2 {
-		fmt.Println("need at least 2 documents to find candidates")
-		os.Exit(0)
-	}
+func shingleSets(sourcesList []string, doKShingle bool) [][]string {
 	fmt.Printf("shingling %d sources:\n", len(sourcesList))
 
 	shingleSets := make([][]string, 0)
@@ -130,7 +108,7 @@ func shingleSets(sources string, doKShingle bool) [][]string {
 func doLSH(cmd *flag.FlagSet) {
 	parseCommand(cmd)
 
-	shingleSets := shingleSets(*lshSources, false)
+	shingleSets := shingleSets(toSourceList(*lshSources), false)
 	if len(shingleSets) < 2 {
 		fmt.Printf("nothing to compare, got %d shingle set(s)\n", len(shingleSets))
 		os.Exit(0)
@@ -150,11 +128,69 @@ func doLSH(cmd *flag.FlagSet) {
 func doSim(cmd *flag.FlagSet) {
 	parseCommand(cmd)
 
-	shingleSets := shingleSets(*simSources, true)
-	if len(shingleSets) != 2 {
+	texts := make([][]string, 0)
+	sourceList := toSourceList(*simSources)
+	for _, s := range sourceList {
+		texts = append(texts, getTextLines(s))
+	}
+
+	if len(texts) != 2 {
 		fmt.Println("you can compare only 2 sets")
 		os.Exit(0)
 	}
 
-	fmt.Printf("similarity: %.4f\n", lsh.Jaccard(shingleSets[0], shingleSets[1]))
+	fmt.Printf("similarity: %.4f\n", lsh.Jaccard(texts[0], texts[1]))
+}
+
+func parseHTML(html []string, verbose bool) []string {
+	// will trim out all the tabs from text
+	hizer, err := htmlizer.New([]rune{'\t'})
+	if err != nil && verbose {
+		fmt.Printf("error in parsing HTML lines: %v\n", err)
+		return []string{}
+	}
+
+	for _, line := range html {
+		err = hizer.Load(line)
+		if err != nil && verbose {
+			fmt.Printf("error in loading line \"%s\": %v\n", line, err)
+		}
+	}
+
+	if verbose {
+		fmt.Println("\nparsed HTML: ")
+		fmt.Printf("%v\n\n", hizer)
+	}
+
+	return strings.Split(hizer.HumanReadable(), "\n")
+}
+
+func getTextLines(source string) []string {
+	reader, err := inout.New(source)
+	if err != nil {
+		fmt.Printf("can't read source %s: %v\n", source, err)
+		return []string{}
+	}
+	lines, err := reader.ReadLines()
+	if err != nil {
+		fmt.Printf("can't fetch contents of %s: %v\n", source, err)
+		return []string{}
+	}
+
+	return parseHTML(lines, false)
+}
+
+func toSourceList(sources string) []string {
+	if sources == "" {
+		printUsage()
+		os.Exit(4)
+	}
+
+	sourcesList := strings.Split(sources, ",")
+	if len(sourcesList) < 2 {
+		fmt.Println("need at least 2 documents to find candidates")
+		os.Exit(0)
+	}
+
+	return sourcesList
 }
